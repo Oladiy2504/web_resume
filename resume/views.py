a81354bcd2ddf22aef56b10d5b0aa87516510352
Максим Копнев <kopnevmaxim@mail.ru>
2025-08-02 21:23:43 +0300

    Structure commit 1
    - Реализована примерная структура сайта
    
    - Разобрался с добавлением своих стилей с автоматическим префиксом

--- resume/views.py
+++ resume/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 
 def home(request):
+    timeline = [
+        {'year': '2018', 'title': 'Окончил университет', 'description': 'Бакалавр в Computer Science'},
+        {'year': '2019–2022', 'title': 'Компания X', 'description': 'Backend-разработчик Python/Django'}
+    ]
+    skills = [
+        {'name': 'Python', 'level': 'Advanced'},
+        {'name': 'Django', 'level': 'Advanced'}
+    ]
+    projects = [
+        {'title': 'MySite', 'description': 'Сайт-портфолио', 'tech': 'Django, Tailwind',
+         'url': 'https://github.com/...'}
+    ]
+    bio_text = "lorem ipsum"
     context = {
         'name': 'Maksim Kopnev',
         'position': 'Backend‑разработчик',
+        'timeline': timeline,
+        'skills': skills,
+        'projects': projects,
+        'bio_text': bio_text
     }
     return render(request, 'resume/home.html', context)
